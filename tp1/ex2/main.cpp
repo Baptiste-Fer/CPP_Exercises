00664d60e93169fc77db68ff1b372aa4f3a40507
Fernandes Baptiste <baptiste.fernandes@edu.univ-eiffel.fr>
2024-01-19 12:11:14 +0100

    ex2-q6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,9 +1,31 @@
 #include "Person.hpp"
+#include <vector>
 
 int main()
 {
+    auto people = std::vector<Person>();
+    
+    std::cout << "Nombre de personnes: ";
+    int nb;
+    std::cin >> nb;
     Person person;
-    person.set_first_name("Paluche");
-    person.set_surname("La Faluche");
-    std::cout << "La personne s'appelle " << person.get_first_name() << " " << person.get_surname() << std::endl;
-}
+    std::string name;
+    std::string surname;
+    while (nb > 0)
+    {
+        std::cout << "Prenom: ";
+        std::cin >> name;
+        std::cout << "Nom: ";
+        std::cin >> surname;
+        person.set_first_name(name);
+        person.set_surname(surname);
+        people.push_back(person);
+        nb--;
+    }
+    std::cout << "Les personnes sont ";
+    for (int i = 0; i < people.size() - 1; i++)
+    {
+        std::cout << people[i].get_first_name() << " " << people[i].get_surname() << ",";
+    }
+    std::cout << people[people.size() - 1].get_first_name() << " " << people[people.size() - 1].get_surname() << "." << std::endl;
+};
